e11120da5a52e63f61f9e11e177f714566bad39c
Dennis Grewe <dennis.grewe@hs-esslingen.de>
2025-10-29 16:44:49 +0100

    re-build deleted feature and step implementations
    Signed-off-by: Dennis Grewe <dennis.grewe@hs-esslingen.de>

--- CalculatorJava/src/test/java/de/hse/swt/steps/CalculatorSteps.java
+++ CalculatorJava/src/test/java/de/hse/swt/steps/CalculatorSteps.java
@@ -7,28 +7,82 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+/**
+ * Step definitions for Calculator BDD feature tests.
+ * 
+ * This class contains all the Cucumber step definitions that map Gherkin
+ * feature file steps to actual test code implementation.
+ */
 public class CalculatorSteps {
 
     private Calculator calculator;
     private double result;
 
-    @Given("I have a Calculator and two numbers")
-    public void i_do_hava_a_calculator_and_two_numbers() {
+    /**
+     * Initializes a new Calculator instance.
+     * Step: "Given I have a Calculator"
+     */
+    @Given("I have a Calculator")
+    public void i_have_a_calculator() {
         this.calculator = new Calculator();
     }
 
-    @When("I perform addition on {double} and {double}")
-    public void i_perform_addition_on_two_numbers(double number1, double number2) {
+    /**
+     * Performs addition operation on two numbers.
+     * Step: "When I add {double} and {double}"
+     * 
+     * @param number1 the first number
+     * @param number2 the second number
+     */
+    @When("I add {double} and {double}")
+    public void i_add_two_numbers(double number1, double number2) {
         this.result = this.calculator.add(number1, number2);
     }
 
-    @When("I perform substraction on {double} and {double}")
-    public void i_perform_substraction_on_two_numbers(double number1, double number2) {
+    /**
+     * Performs subtraction operation on two numbers.
+     * Step: "When I subtract {double} and {double}"
+     * 
+     * @param number1 the first number (minuend)
+     * @param number2 the second number (subtrahend)
+     */
+    @When("I subtract {double} and {double}")
+    public void i_subtract_two_numbers(double number1, double number2) {
         this.result = this.calculator.subtract(number1, number2);
     }
 
+    /**
+     * Performs multiplication operation on two numbers.
+     * Step: "When I multiply {double} and {double}"
+     * 
+     * @param number1 the first number
+     * @param number2 the second number
+     */
+    @When("I multiply {double} and {double}")
+    public void i_multiply_two_numbers(double number1, double number2) {
+        this.result = this.calculator.multiply(number1, number2);
+    }
+
+    /**
+     * Performs division operation on two numbers.
+     * Step: "When I divide {double} and {double}"
+     * 
+     * @param number1 the dividend
+     * @param number2 the divisor
+     */
+    @When("I divide {double} and {double}")
+    public void i_divide_two_numbers(double number1, double number2) {
+        this.result = this.calculator.divide(number1, number2);
+    }
+
+    /**
+     * Verifies that the calculated result matches the expected value.
+     * Step: "Then the result should be {double}"
+     * 
+     * @param expected the expected result value
+     */
     @Then("the result should be {double}")
-    public void i_receive_the_result(double expected) {
+    public void the_result_should_be(double expected) {
         assertEquals(expected, this.result, 0.0001);
     }
 }
--- CalculatorJava/src/test/resources/features/Calculator.feature
+++ CalculatorJava/src/test/resources/features/Calculator.feature
@@ -5,6 +5,21 @@ Feature: Calculator Operations
   So that I can calculate single expressions
 
   Scenario: Add two numbers
-    Given I have a Calculator and two numbers
-    When I perform addition on 5 and 7
-    Then the result should be 12
+    Given I have a Calculator
+    When I add 5 and 3
+    Then the result should be 8
+
+  Scenario: Subtract two numbers
+    Given I have a Calculator
+    When I subtract 4 and 2
+    Then the result should be 2
+
+  Scenario: Multiply two numbers
+    Given I have a Calculator
+    When I multiply 4 and 2
+    Then the result should be 8
+
+  Scenario: Divide two numbers
+    Given I have a Calculator
+    When I divide 4 and 2
+    Then the result should be 2
